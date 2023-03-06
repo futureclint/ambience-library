37afea5ebffbb5bc3e87a75314777f7c49798e2f
Devon Moubry <devon@moubry.com>
2023-03-06 17:01:57 -0600

    renders one static video

---
+++ src/components/VideoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="videos-list">
+    <h1>Ambience Library</h1>
+    <div class="list-wrap">
+      <div>
+        <div key="fV1kXBfMl6k">
+          <div>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/fV1kXBfMl6k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          </div>
+          <span>Tags: Underwater</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
