d8a14afade858b4b41868a29851a6a88c8c0a530
Devon Moubry <devon@moubry.com>
2023-03-06 18:25:48 -0600

    renders videos from static list with v-for

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -2,14 +2,32 @@
   <div class="videos-list">
     <h1>Ambience Library</h1>
     <div class="list-wrap">
-      <div>
-        <div key="fV1kXBfMl6k">
-          <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/fV1kXBfMl6k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          </div>
-          <span>Tags: Underwater</span>
+      <div v-for="video in videos" :key="video.id" :id="video.id">
+        <div class="video-wrap">
+            <iframe width="560" height="315" :src="video.source" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
-      </div>
+        <div class="tag-wrap">
+          <div>Tags:</div>
+          <span v-for="tag in video.tags">{{ tag }}</span>
+        </div>
+    </div>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+  const videos = [
+    { 
+      id: 'pjeaX4Z2N6Q', 
+      source: 'https://www.youtube.com/embed/pjeaX4Z2N6Q', 
+      tags: ['work', 'jazz'] 
+    }, { 
+      id: 'nd5cpmQiP-4', 
+      source: 'https://www.youtube.com/embed/nd5cpmQiP-4', 
+      tags: ['cozy', 'hobbit', 'coffee shop']
+    }, { 
+      id: 'fV1kXBfMl6k', 
+      source: 'https://www.youtube.com/embed/fV1kXBfMl6k', 
+      tags: ['underwater', 'ambient']
+    }
+  ]
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ListView from '../views/ListView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,7 @@ const router = createRouter({
     {
       path: '/list',
       name: 'list',
-      component: () => import('../views/ListView.vue')
+      component: ListView
     }
   ]
 })
